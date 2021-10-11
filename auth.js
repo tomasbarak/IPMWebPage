541a733adad2bfcdabc74c6edf75f835484a3376
tomasbarak <barakcinthia@gmail.com>
2021-10-11 03:25:22 -0300

    Added cookie support for auth

--- auth.js
+++ auth.js
@@ -1,13 +1,12 @@
 function auth(code){
     var req = new XMLHttpRequest()
         setLoading(true);
-        req.open('GET', "https://ipmalumns.herokuapp.com/auth?code=" + code)
+        //req.open('POST', "https://ipmalumnstrimbackups.herokuapp.com/auth?code=" + code)
+        req.open('POST', "http://127.0.0.1:3030/auth?code=" + code)
         req.onreadystatechange = function () {
             if (req.readyState === 4) {
                 if (req.status === 200) {
-                    console.log(this.response)
-                    sessionStorage.setItem('authorized', "yes");
-                    window.location = "search.html"
+                    console.log(this)
                 }
                 else {
                     console.log("Error loading page\n");
@@ -15,6 +14,5 @@ function auth(code){
             }
             setLoading(false);
         };
-
         req.send();
 }
